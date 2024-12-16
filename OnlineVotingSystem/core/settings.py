91a07f89201a7236be5bd9d6bcd91ff5475eecc7
EduardoAcu <eduardoignacio577@gmail.com>
2024-12-16 16:19:11 -0300

    Update settings.py

--- OnlineVotingSystem/core/settings.py
+++ OnlineVotingSystem/core/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': 'e_voting',
         'USER': 'acuna',
         'PASSWORD': 'admin123',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
