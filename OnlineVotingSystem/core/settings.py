faa726f8e7c7ba2891a61d4546908ee8130dc942
EduardoAcu <eduardoignacio577@gmail.com>
2024-12-16 23:09:33 -0300

    Update settings.py

--- OnlineVotingSystem/core/settings.py
+++ OnlineVotingSystem/core/settings.py
@@ -113,7 +113,7 @@ STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GOOGLE_BUCKET_NAME = 'django-voting'
 
 client = secretmanager.SecretManagerServiceClient()
-secret_name = "projects/1087274725247/secrets/django-key/versions/latest"
+secret_name = ['projects/1087274725247/secrets/django-key/versions/2']
 response = client.access_secret_version(name=secret_name)
 secret_data = response.payload.data.decode("UTF-8")
 
